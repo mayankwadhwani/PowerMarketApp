b92d962c3a4d72ef2ee0f6dd6c0975df5fd8982c
Lucas Porter-Bakker <lucas.porter.bakker@gmail.com>
2020-09-15 17:05:23 -0400

    sample report with paywall routing

--- resources/views/pages/reporting.blade.php
+++ resources/views/pages/reporting.blade.php
@@ -4,11 +4,11 @@
     <div class="header bg-gradient-primary py-7 py-lg-8 pt-lg-9">
         <div class="container">
             <div class="header-body text-center mb-7">
-                <div class="row justify-content-center">
+                <!-- <div class="row justify-content-center">
                     <div class="col-xl-5 col-lg-6 col-md-8 px-5">
                         <h1 class="text-white">{{ __('Detailed Report') }}</h1>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
         <div class="separator separator-bottom separator-skew zindex-100">
@@ -19,5 +19,29 @@
     </div>
     <!-- Page content -->
     <div class="container mt--8 pb-5">
+      <div class="container mt--8 pb-5">
+          <div class="row justify-content-center">
+              <div class="col-lg-10">
+                  <div class="pricing card-group flex-column flex-md-row mb-3">
+                      <div class="card card-pricing border-0 text-center mb-4">
+                          <div class="card-header bg-transparent">
+                              <h4 class="text-uppercase ls-1 text-primary py-3 mb-0">Sample Report</h4>
+                          </div>
+                          <div class="card-body px-lg-7">
+                            <img src="{{ asset('argon') }}/img/theme/PMD.png" width="100%" class="" alt="...">
+
+                              <a href="{{ route('page.pricing') }}"><button type="button" class="btn btn-primary mb-3">Upgrade to Download Reports</button></a>
+                          </div>
+                          <!-- <div class="card-footer">
+                              <a href="mailto: abhinav.jain@powermarket.net" target="_blank" class="text-light">Upgrade to generate your own</a>
+                          </div> -->
+                      </div>
+
+                  </div>
+              </div>
+          </div>
+
+
+      </div>
     </div>
 @endsection
