04ca8338f82495edb3842622e9c532fc309a926e
Lucas Porter-Bakker <lucas.porter.bakker@gmail.com>
2020-09-16 13:28:28 -0400

    privacy policy

--- resources/views/pages/privacy.blade.php
+++ resources/views/pages/privacy.blade.php
@@ -4,11 +4,11 @@
     <div class="header bg-gradient-primary py-7 py-lg-8 pt-lg-9">
         <div class="container">
             <div class="header-body text-center mb-7">
-                <div class="row justify-content-center">
+                <!-- <div class="row justify-content-center">
                     <div class="col-xl-5 col-lg-6 col-md-8 px-5">
                         <h1 class="text-white">{{ __('Privacy Policy') }}</h1>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
         <div class="separator separator-bottom separator-skew zindex-100">
@@ -19,15 +19,164 @@
     </div>
     <!-- Page content -->
     <div class="container mt--8 pb-5">
-        <div class="d-flex justify-content-lg-center px-3 mt-8 mb-6">
-{{--            <div>--}}
-{{--                <div class="icon icon-shape bg-gradient-white shadow rounded-circle text-primary">--}}
-{{--                    <i class="ni ni-check-bold text-primary"></i>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-            <div class="col-lg-6">
-                <p class="text-white">PowerMarket is 100% compliant with the General Data Protection Regulation.</p>
-            </div>
-        </div>
+      <div class="container mt--8 pb-5">
+          <div class="row justify-content-center">
+              <div class="col-lg-10">
+                  <div class="pricing card-group flex-column flex-md-row mb-3">
+                      <div class="card card-pricing border-0 text-center mb-4">
+                          <div class="card-header bg-transparent">
+                              <h4 class="text-uppercase ls-1 text-primary py-3 mb-0">Beta Evaluation Licence and Non-Disclosure Agreement</h4>
+                          </div>
+                          <div class="card-body px-lg-7">
+
+<h5 style="text-align: left;">Parties</h5>
+<p style="text-align: left;">
+Thank you for your interest in being a Beta tester for SunReign Ltd - Trading as PowerMarket.
+<br>
+<br>
+As with any Beta test program, you (The Company) will be asked to agree to several conditions before you begin testing materials. Don't worry, we're not asking for much! Here's a summary of the main terms you'll be agreeing to:
+<br>
+<br>
+1. The Company acknowledges that its participating in a PowerMarket Beta program and the info The Company learns as a tester is confidential and cannot be shared.
+<br>
+<br>
+2. The information The Company provided in the PowerMarket Beta survey is accurate.
+<br>
+<br>
+3. The Company is not a member of the press, nor an employee or representative of any competitor.
+<br>
+<br>
+4. The Company won't disclose its role or work as a Beta tester to any member of the press, any public news group, or any third party.
+<br>
+<br>
+5. The Company will reasonably respond in a timely manner to PowerMarket’s requests for feedback.
+<br>
+<br>
+6. The Company licenses PowerMarket to freely use the feedback and suggestions it will provide.
+<br>
+<br>
+IF THE COMPANY AGREES TO THESE CONDITIONS, PLEASE READ THE TERMS OF THIS BETA TESTER AND CONFIDENTIALITY AGREEMENT ("AGREEMENT") GOVERNING THE USE OF THE SOFTWARE, RELATED FIRMWARE AND HARDWARE PROVIDED TO YOU BY SUNREIGN LTD. ("POWERMARKET").
+BY INDICATING ITS ACCEPTANCE OF THESE TERMS AND SELECTING THE "I agree with the Privacy Policy" BUTTON AS IT APPEARS IN THE POWERMARKET APP, ON ACCOUNT CREATION, OR IN THE BETA SURVEY, THE COMPANY AND ITS REPRESENTATIVE (1) IS OVER THE AGE OF 18, (2) REPRESENTING THAT YOU HAVE THE RIGHT AND AUTHORITY TO LEGALLY BIND YOURSELF TO THESE TERMS, AND (3) CONSENTING TO BE LEGALLY BOUND BY ALL OF THE TERMS OF THIS AGREEMENT. THIS WILL CREATE A BINDING AGREEMENT. IF THE COMPANY DOES NOT AGREE TO ALL THESE TERMS OR CANNOT MAKE SUCH REPRESENTATIONS SELECT THE "I DO NOT AGREE" BUTTON AS IT APPEARS IN THE POWERMARKET APP OR IN THE SURVEY AND DO NOT USE THE PRODUCTS OR MATERIALS.
+<br>
+<br>
+</p>
+
+<h5 style="text-align: left;">Background</h5>
+<p style="text-align: left;">
+PowerMarket is developing a web-app to provide solar insights leveraging earth observation data and deep-learning. (“Platform”).
+PowerMarket has agreed to provide the Company with access to the Platform and certain information owned by PowerMarket and obtained by the Company relating to the Platform (however recorded or preserved and in any form or medium) (“PowerMarket Information”) for the purposes of evaluating the Platform for use in the Company’s business (“Objective”) on the terms and subject to the conditions set out in this Agreement.
+The Company will have access to the Platform for a maximum of 12 months.
+<br>
+<br>
+Access for the Company’s representatives will be granted via an email and password.
+Company will pay a one off, non-refundable fee as agreed upon in a separate commercial agreement to help with the costs of providing this service for this period.
+<br>
+<br>
+</p>
+
+<h5 style="text-align: left;">License</h5>
+<p style="text-align: left;">
+PowerMarket hereby grants to the Company, a personal, non-transferable, non-exclusive licence to access and use the Platform for a period of up to 12 months (“Evaluation Period”) solely for the purposes of the Objective.
+The Company acknowledges and agrees that the Platform may automatically “time out” (that is to say, cease to operate) at the end of the Evaluation Period if the Company has not at that time indicated a firm intention to accept or has accepted a full licence of the Platform.
+<br>
+<br>
+During the Evaluation Period this licence may be terminated immediately by PowerMarket giving written notice if the Company is in breach of any of its obligations under this Agreement. The licence may be terminated by the Company during the Evaluation Period upon seven (7) days’ written notice or upon acceptance by the Company of a full licence for the Platform.
+<br>
+<br>
+Following termination of this licence however arising, PowerMarket may, at its sole discretion, provide the Company with a copy of any content or data uploaded to or generated by the Platform as a result of the Company’s use during the Evaluation Period. Such content or data shall be provided in such format and on such media or by such means as PowerMarket shall determine.
+<br>
+<br>
+The Company acknowledges and agrees that the Platform is an “beta version” and is provided to the Company on an “as is” and “as available” basis. PowerMarket does not guarantee that access to and use of the Platform will be uninterrupted or error-free or that PowerMarket will correct any or all errors in the Platform identified by the Company. No representations, conditions, warranties or other terms of any kind are given in respect of the Platform or the PowerMarket Information, and all statutory warranties and conditions are excluded to the fullest extent possible.
+<br>
+<br>
+The Company is solely responsible for any and all content uploaded to or created within the Platform and agrees to defend PowerMarket against any claim and fully indemnify PowerMarket from any damages, liabilities, costs and expenses arising out of or in connection with any such content.
+<br>
+<br>
+The Company undertakes that it will not upload any content or data to the Platform comprising “personal data” (as defined in article 4(1) of Regulation (EU) 2016/679 (the General Data Protection Regulation)) such that the Company would be rendered a processor as defined under such legislation.
+<br>
+<br>
+Except for any liability which cannot be excluded or limited by applicable law, PowerMarket shall have no liability of any kind in any circumstances whatever to the Company in respect of the Platform or any PowerMarket Information greater than the limit set out in clause 1.9. In particular, PowerMarket shall have no liability in any circumstances whatever for any data loss or corruption and the Company agrees that it has sole responsibility for protecting its data during evaluation of the Platform.
+<br>
+<br>
+Subject to clause 1.8. in the event that PowerMarket shall be found liable to the Company for any reason, the sums payable to the Company in respect of such liability shall not in any circumstances exceed £500 GBP.
+<br>
+<br>
+</p>
+
+<h5 style="text-align: left;">Non-disclosure agreement</h5>
+<p style="text-align: left;">
+In consideration of the mutual promises and obligations set out in this Agreement:
+<br>
+<br>
+the Company undertakes that it will respect and preserve the confidentiality of the PowerMarket Information and that it will not use or exploit in any way the PowerMarket Information, except for or in connection with the Objective, for a period of ten years after the date of such disclosure (subject to clause 2.3 below); and
+PowerMarket undertakes that it will respect and preserve the confidentiality of any confidential or proprietary information or trade secrets (however recorded or preserved and in any form or medium) relating to the Company which has been disclosed to or obtained by PowerMarket in connection with this Agreement which is labelled as such or which should reasonably be considered confidential because of its nature and the manner of its disclosure (“Company Information”).
+<br>
+<br>
+Neither party shall, without the prior written consent of the other party:
+communicate or otherwise make available the PowerMarket Information or Company Information (as appropriate) to any third party; or
+use the PowerMarket Information or Company Information (as appropriate) itself for any commercial, industrial or other purpose other than in connection with the Objective; or
+copy, adapt, or otherwise reproduce the PowerMarket Information or Company Information (as appropriate) save as strictly necessary for the purposes of the Objective.
+<br>
+<br>
+The Company may disclose the Platform and Information or any part thereof to such of its personnel who need access to the Platform and the Information in connection with the Objective. In such an event the Company agrees to ensure, before such disclosure, that such personnel are made aware of the confidential nature of the Platform and Information and understands that they are bound by conditions of secrecy no less strict than those set out in this Agreement. The Company agrees to monitor the use of the Platform and Information by its personnel and to enforce their obligations of confidence at the request of PowerMarket.
+<br>
+<br>
+The obligations contained in this clause 2 shall not apply, or shall cease to apply, to such part of the PowerMarket Information or Company Information (as appropriate) as the party that disclosed it can show to the reasonable satisfaction of party that received it:
+has become public knowledge other than through the fault of that party or those of its personnel to whom it has been disclosed;
+was already known prior to its disclosure; or
+has been received from a third party who neither acquired it in confidence nor owed the party that disclosed it a duty of confidence in respect of it.
+<br>
+<br>
+If the Company decides not to accept a full licence of the Platform, or otherwise at any time at the request of PowerMarket, the Company shall immediately return to PowerMarket all copies of all or any part of the Information which have been provided to the Company pursuant to this Agreement, together with all analyses, studies and other materials produced by the Company which contain, or could reveal, all or any part of the Information, and any summaries (in whatever form) prepared by the Company of oral Information disclosed by PowerMarket.
+<br>
+<br>
+</p>
+
+<h5 style="text-align: left;">Property Rights</h5>
+<p style="text-align: left;">
+PowerMarket owns the Platform, Information and all related documentation. The Company acknowledges that any disclosure pursuant to this Agreement shall not confer on the Company any intellectual property or other rights in relation to the Platform or the Information other than its right to access and use the Platform under clause 1.1.
+If a third party notifies the Company of any claim that the use of the Platform infringes any right of a third party, the Company agrees to immediately notify PowerMarket. If any such claim is made to the Company or PowerMarket, the Company shall, at PowerMarket’s request, immediately cease use of the Platform. If PowerMarket is unable to allow the Company to continue evaluation of the Platform, the provisions of clause 1.3 shall apply.
+<br>
+<br>
+</p>
+
+<h5 style="text-align: left;">General</h5>
+<p style="text-align: left;">
+Neither party shall export, directly or indirectly, any technical data acquired from the other party under this Agreement (or any products, including software, incorporating any such data) in breach of any applicable laws or regulations (“Export Control Laws”), including export laws and regulations in the United Kingdom or United States, to any country for which the United Kingdom, United States or any other government or any agency thereof at the time of export, requires an export licence or other governmental approval without first obtaining such licence or approval.
+<br>
+<br>
+The person agreeing to this Agreement on behalf of the Company confirms that they are authorised to enter into this Agreement on the Company’s behalf, and to bind the Company to its terms and conditions.
+<br>
+<br>
+No variation of this Agreement shall be effective unless it is in writing and signed by or on behalf of both parties.
+<br>
+<br>
+Any reference to “writing” or “written” includes e-mail but not fax or any other form of electronic communication except where expressly provided to the contrary.
+
+<br>
+<br>
+This Agreement and any dispute or claim arising out of or in connection with it or its subject matter or formation (including non-contractual disputes or claims) shall be governed by and construed in accordance with the law of England and Wales. However, PowerMarket may enforce the Company’s obligation of confidence in the courts of any jurisdiction having competence to issue an injunction directly enforceable against the Company.
+<br>
+<br>
+
+<strong>This agreement has been entered into on the date of account creation should you wish to proceed.</strong>
+<br>
+<br>
+</p>
+
+
+                              <a href="{{ route('page.pricing') }}"><button type="button" class="btn btn-primary mb-3">Questions about the Beta Agreement?</button></a>
+                          </div>
+                          <!-- <div class="card-footer">
+                              <a href="mailto: abhinav.jain@powermarket.net" target="_blank" class="text-light">Upgrade to generate your own</a>
+                          </div> -->
+                      </div>
+
+                  </div>
+              </div>
+          </div>
+
+
+      </div>
     </div>
 @endsection
