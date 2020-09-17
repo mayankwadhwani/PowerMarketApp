4f18bc45aa1ab82950e4cff61bf623279bb4014f
Pankaj Batra <pankajb@gmail.com>
2020-09-17 21:22:12 +0530

    fixing intercom script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,28 +67,16 @@
         <script src="{{ asset('argon') }}/js/demo.min.js"></script>
 
         <script>
-      window.intercomSettings = {
-        app_id: "z30flz8e"
-      };
-    </script>
-
-    <script>
-    // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/z30flz8e'
-    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/z30flz8e';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-    </script>
-
-    <script>
-      window.intercomSettings = {
-        app_id: "z30flz8e",
-        name: "<%= current_user.name %>", // Full name
-        email: "<%= current_user.email %>", // Email address
-        created_at: "<%= current_user.created_at.to_i %>" // Signup date as a Unix timestamp
-      };
-    </script>
-
-    <script>
-    // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/z30flz8e'
-    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/z30flz8e';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-    </script>
+            window.intercomSettings = {
+                app_id: "z30flz8e",
+                name: "{{ auth()->user()->name }}", //  name
+                email: "{{ auth()->user()->email }}", // Email address
+                created_at: {{ auth()->user()->created_at->timestamp }} // Signup date as a Unix timestamp
+            };
+        </script>
+        <script>
+            // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/z30flz8e'
+            (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/z30flz8e';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+        </script>
     </body>
 </html>
