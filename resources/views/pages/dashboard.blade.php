456820ccd0b96ee6ee65c69e10a0389edfe9f150
Pankaj Batra <pankajb@gmail.com>
2020-10-08 12:01:05 +0530

    on zooming out, cluster points

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -396,7 +396,10 @@
                     'data': {
                         'type': 'FeatureCollection',
                         'features': features
-                    }
+                    },
+                    cluster: true,
+                    clusterMaxZoom: 12, // Max zoom to cluster points on
+                    clusterRadius: 50
                 });
             features.forEach(function(feature) {
                 var symbol = feature.properties['years'];
@@ -494,6 +497,49 @@
                     });
                 }
             });
+            map.addLayer({
+                id: 'clusters',
+                type: 'circle',
+                source: 'places',
+                filter: ['has', 'point_count'],
+                paint: {
+                        // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+                        // with three steps to implement three types of circles:
+                        //   * Blue, 20px circles when point count is less than 100
+                        //   * Yellow, 30px circles when point count is between 100 and 750
+                        //   * Pink, 40px circles when point count is greater than or equal to 750
+                        'circle-color': '#51bbd6',
+                        // 'circle-color': [
+                        //     'step',
+                        //     ['get', 'point_count'],
+                        //     '#51bbd6',
+                        //     100,
+                        //     '#f1f075',
+                        //     750,
+                        //     '#f28cb1'
+                        // ],
+                        'circle-radius': [
+                            'step',
+                            ['get', 'point_count'],
+                            20,
+                            100,
+                            30,
+                            750,
+                            40
+                        ]
+                    }
+                });
+                map.addLayer({
+                    id: 'cluster-count',
+                    type: 'symbol',
+                    source: 'places',
+                    filter: ['has', 'point_count'],
+                    layout: {
+                        'text-field': '{point_count_abbreviated}',
+                        'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+                        'text-size': 12
+                    }
+                });
             // map.addLayer({
             //     'id': 'places',
             //     'type': 'circle',
