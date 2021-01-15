938bc3718fd8580a1627af885f5c46fb736dddde
Nazrulla Sadullaev <nsadullaev98@gmail.com>
2021-01-15 20:43:25 +0500

    Add database schema for clusters

---
+++ database/migrations/2021_01_15_151730_create_cluster_geopoint_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClusterGeopointTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cluster_geopoint', function (Blueprint $table) {
+            $table->foreignId('cluster_id');
+            $table->foreignId('geopoint_id');
+            $table->primary(['cluster_id', 'geopoint_id']);
+            $table->foreign('cluster_id')->references('id')->on('clusters')->onDelete('cascade');
+            $table->foreign('geopoint_id')->references('id')->on('geopoints')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cluster_geopoint');
+    }
+}
