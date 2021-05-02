18f3b9ff8dde4a5e98973373e3aeed7e67f00d56
Nazrulla Sadullaev <nsadullaev98@gmail.com>
2021-05-02 20:14:00 +0500

    Add tildo sign instead of quote in reporting blade

--- resources/views/pages/reporting.blade.php
+++ resources/views/pages/reporting.blade.php
@@ -382,8 +382,8 @@
         ' !!}');
 
     function renderTable() {
-        var jsonString = '{!! $geodata ?? '
-        ' !!}';
+        var jsonString = `{!! $geodata ?? '
+        ' !!}`;
         if (jsonString.length > 0) {
             //var jsonData = JSON.parse(jsonString);
             var dataArray = JSON.parse(jsonString);
