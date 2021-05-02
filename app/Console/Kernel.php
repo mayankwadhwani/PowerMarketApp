5458def49b7048c2752ed467eb8403fa0307dd1a
Nazrulla Sadullaev <nsadullaev98@gmail.com>
2021-05-02 19:33:44 +0500

    Add a geocoding fixing script

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,10 @@
 
 namespace App\Console;
 
+use App\Geopoint;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,8 +28,38 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        //fix geocoding errors
+        $schedule->call(function () {
+            //regions that need fixing
+            $region_ids = [];
+
+            $geopoints = Geopoint::whereIn('region_id', $region_ids)->get();
+            $key = config('services.google_maps.key');
+            $cases = [];
+            $ids = [];
+            $values = [];
+            foreach ($geopoints as $geopoint) {
+                $lat = $geopoint->latLon->getLat();
+                $lon = $geopoint->latLon->getLng();
+                try {
+                    $response = Http::get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' . $lat . ',' . $lon . '&key=' . $key);
+                    $address = $response['results'][0]['formatted_address'];
+                    $cases[] = "WHEN {$geopoint->id} then ?";
+                    $values[] = $address;
+                    $ids[] = $geopoint->id;
+                } catch (\Exception $e) {
+                    $address = null;
+                    Log::channel('geocodinglog')->critical($e->getMessage());
+                }
+            }
+            $ids = implode(',', $ids);
+            $cases = implode(' ', $cases);
+
+            if (!empty($ids)) {
+                DB::update("UPDATE geopoints SET `address` = CASE `id` {$cases} END
+            WHERE `id` in ({$ids})", $values);
+            }
+        })->everyMinute();
     }
 
     /**
@@ -35,7 +69,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
