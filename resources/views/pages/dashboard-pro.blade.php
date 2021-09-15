7c34551fe128579a12a39066f5dfc8bdf1da1a1f
root <root@toptal.mishutin.nl>
2021-09-15 22:47:09 +0200

    Fix: 100 in captive refreshes to 1

--- resources/views/pages/dashboard-pro.blade.php
+++ resources/views/pages/dashboard-pro.blade.php
@@ -509,7 +509,7 @@ div#calculated-area {
                   <div class="col-sm-2 form-group{{ $errors->has('captive-use') ? ' has-danger' : '' }}">
                     <label class="form-control-label" for="input-captive-use">{{ __('Captive Use') }} <img src="{{ asset('svg') }}/info.svg" style="width: 10px; margin-bottom: 15px;"data-toggle="tooltip" title="Captive use." /></label>
 
-                    <input type="number" step="any" name="captive_use" id="input-captive-use" class="pro-input form-control{{ $errors->has('captive_use') ? ' is-invalid' : '' }}" placeholder="<?php echo $orgdata['captiveuse']; ?>" value="{{ ($prev_inputs['captive_use'] < 1 ? $prev_inputs['captive_use']*100 : $prev_inputs['captive_use'] ) }}">
+                    <input type="number" step="any" name="captive_use" id="input-captive-use" class="pro-input form-control{{ $errors->has('captive_use') ? ' is-invalid' : '' }}" placeholder="<?php echo $orgdata['captiveuse']; ?>" value="{{ ($prev_inputs['captive_use'] <= 1 ? $prev_inputs['captive_use']*100 : $prev_inputs['captive_use'] ) }}">
                     @include('alerts.feedback', ['field' => 'captive_use'])
                   </div>
                   <div class="col-sm-2 form-group{{ $errors->has('export-tariff') ? ' has-danger' : '' }}">
